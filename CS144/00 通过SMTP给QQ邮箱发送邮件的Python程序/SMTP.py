565912492aba7a4c4d04e4182fc9674041891c28
Huoyuuu <1013239408@qq.com>
2023-02-01 14:23:19 +0800

    update：通过SMTP给QQ邮箱发送邮件的Python程序

---
+++ CS144/00 通过SMTP给QQ邮箱发送邮件的Python程序/SMTP.py
@@ -0,0 +1,26 @@
+import smtplib
+import configparser
+from email.mime.text import MIMEText
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+sender_email = config['email']['sender']
+bcc_emails = config['email']['bcc'].split(",")
+password = config['email']['password']
+
+message = MIMEText("Hello World")
+message['Subject'] = "Hi there"
+message['From'] = sender_email
+
+try:
+    server = smtplib.SMTP('smtp.qq.com', 25)
+    server.ehlo()
+    server.starttls()
+    server.login(sender_email.split("<")[1][:-1], password)
+    server.sendmail(sender_email, bcc_emails, message.as_string())
+    print('Email sent!')
+except Exception as e:
+    print('Something went wrong...', e)
+finally:
+    server.quit()
