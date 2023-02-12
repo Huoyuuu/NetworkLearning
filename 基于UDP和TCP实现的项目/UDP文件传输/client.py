09feaa022dd52493ae30363f74c91c270411398d
Huoyuuu <1013239408@qq.com>
2023-02-12 16:52:13 +0800

    update: 添加SHA-256校验

--- 基于UDP和TCP实现的项目/UDP文件传输/client.py
+++ 基于UDP和TCP实现的项目/UDP文件传输/client.py
@@ -1,7 +1,20 @@
 from socket import *
 from tqdm import tqdm
+import sys
 import json
 import base64
+import hashlib
+
+
+def sha256_hash_of_file(filename):
+    sha256 = hashlib.sha256()
+    with open(filename, 'rb') as file:
+        while True:
+            chunk = file.read(4096)
+            if not chunk:
+                break
+            sha256.update(chunk)
+    return sha256.hexdigest()
 
 
 def split_file_into_packets(file, packet_size):
@@ -25,17 +38,38 @@ def merge_packets_to_file(packets, file):
             f.write(packet[1])
 
 
+def check_SHA_256(server_SHA_256, client_SHA_256):
+    print(f"发送端SHA-256为{client_SHA_256}")
+    print(f"接收端SHA-256为{server_SHA_256}")
+    if(client_SHA_256 == server_SHA_256):
+        print("传输无误")
+    else:
+        print("传输结果异常")
+
+
 if __name__ == "__main__":
     serverName = "127.0.0.1"
     serverPort = 12000
     clientSocket = socket(AF_INET, SOCK_DGRAM)
+    target = (serverName, serverPort)
 
-    packets = split_file_into_packets('示例文件.jpg', 1024)
-    clientSocket.sendto(str(len(packets)).encode(), (serverName, serverPort))
-    print("总长度为", len(packets))
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+    else:
+        file_name = "示例文件.jpg"
+    packets = split_file_into_packets(file_name, 1024)
+    client_SHA_256 = sha256_hash_of_file(file_name)
+    clientSocket.sendto(str(len(packets)).encode(), target)
+    clientSocket.sendto(file_name.encode(), target)
+    clientSocket.sendto(client_SHA_256.encode(), target)
+    print(f"总长度为{len(packets)}")
+    print(f"文件名为{file_name}")
     encoded_byte_array = [(x[0], base64.b64encode(x[1]).decode())
                           for x in packets]
     for packet in tqdm(encoded_byte_array):
         clientSocket.sendto(json.dumps(packet).encode(
-            'utf-8'), (serverName, serverPort))
+            'utf-8'), target)
+    server_SHA_256, addr = clientSocket.recvfrom(1024)
+    server_SHA_256 = server_SHA_256.decode()
+    check_SHA_256(server_SHA_256, client_SHA_256)
     clientSocket.close()
--- 基于UDP和TCP实现的项目/UDP文件传输/server.py
+++ 基于UDP和TCP实现的项目/UDP文件传输/server.py
@@ -2,6 +2,18 @@ from socket import *
 from tqdm import tqdm
 import json
 import base64
+import hashlib
+
+
+def sha256_hash_of_file(filename):
+    sha256 = hashlib.sha256()
+    with open(filename, 'rb') as file:
+        while True:
+            chunk = file.read(4096)
+            if not chunk:
+                break
+            sha256.update(chunk)
+    return sha256.hexdigest()
 
 
 def split_file_into_packets(file, packet_size):
@@ -25,6 +37,15 @@ def merge_packets_to_file(packets, file):
             f.write(packet[1])
 
 
+def check_SHA_256(server_SHA_256, client_SHA_256):
+    print(f"发送端SHA-256为{client_SHA_256}")
+    print(f"接收端SHA-256为{server_SHA_256}")
+    if(client_SHA_256 == server_SHA_256):
+        print("传输无误")
+    else:
+        print("传输结果异常")
+
+
 if __name__ == "__main__":
     serverPort = 12000
     serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -33,11 +54,20 @@ if __name__ == "__main__":
     while True:
         new_packets = []
         length, addr = serverSocket.recvfrom(1024)
+        file_name, addr = serverSocket.recvfrom(1024)
+        client_SHA_256, addr = serverSocket.recvfrom(1024)
         length = int(length.decode())
-        print("总长度为", length)
+        file_name = file_name.decode()
+        client_SHA_256 = client_SHA_256.decode()
+        print(f"总长度为{length}")
+        print(f"文件名为{file_name}")
         for i in tqdm(range(length)):
             message, addr = serverSocket.recvfrom(2048)
             new_packets.append(json.loads(message.decode('utf-8')))
         new_packets = [
             [x[0], base64.b64decode(x[1].encode())] for x in new_packets]
-        merge_packets_to_file(new_packets, "输出结果.jpg")
+        merge_packets_to_file(new_packets, "接受到的" + file_name)
+
+        server_SHA_256 = sha256_hash_of_file("接受到的" + file_name)
+        serverSocket.sendto(server_SHA_256.encode(), addr)
+        check_SHA_256(server_SHA_256, client_SHA_256)
